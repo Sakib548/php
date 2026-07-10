58a8a655d594b5ea636387289e1248367cdb6d22
Sakib548 <mdsakibhassan548@gmail.com>
2026-07-10 09:56:06 +0600

    Php_Router

---
+++ 2.dynamic_web_app/2.4.PHP_Router/views/index.view.php
@@ -0,0 +1,10 @@
+<?php require("partials/head.php"); ?>
+
+
+    
+<?php require("partials/banner.php"); ?>
+
+<?php require("partials/main.php");?>       
+<?=   $basic_url  ;   ?>
+
+<?php require("partials/footer.php"); ?>
